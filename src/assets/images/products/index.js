47240658d7522c9899bf66aaad0233309c8a4e2c
hoxxor <nhannt.gm@gmail.com>
2022-08-27 21:31:41 +0700

    chore:add images

---
+++ src/assets/images/products/index.js
@@ -0,0 +1,13 @@
+const products = {
+  charms: require('./charms.webp'),
+  enamelPin: require('./enamel-pin.webp'),
+  mousepad: require('./mousepad.webp'),
+  posterKanekiru: require('./poster-kanekiru.webp'),
+  posterNaizotan: require('./poster-naizotan.webp'),
+  posterYampa: require('./poster-yampa.webp'),
+  stickerA: require('./sticker-a.webp'),
+  stickerB: require('./sticker-b.webp'),
+  tShirt: require('./t-shirt.webp')
+}
+
+export default products
---
+++ src/assets/images/wallpapers/index.js
@@ -0,0 +1,6 @@
+const wallpapers = {
+  wallpaper_1: require('./wallpaper-1.webp'),
+  wallpaper_2: require('./wallpaper-2.webp'),
+}
+
+export default wallpapers
