a11329d57c4c7ad95b72ee161a4e3300cd823b1b
hoxxor <nhannt.gm@gmail.com>
2022-09-14 01:16:50 +0700

    chore: change display flex to display grid

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,16 +1,40 @@
-const options = ["Home", "Products", "About", "FAQ", "Contact", "Cart"];
+import axios from 'axios'
+
+const options = ['Home', 'Products', 'About', 'FAQ', 'Contact', 'Cart']
+
+var client_id = '593cf08483ba4c23bec8021016be638d'; // Your client id
+var client_secret = '62810795b365430aa9785fdc47737fce'; // Your secret
+var redirect_uri = 'http://localhost:3000/'; // Your redirect uri
+// var scopes = \'user-read-private user-read-email\';
+
+axios
+  .get('https://api.spotify.com/v1/', {
+    params: {
+      response_type: 'code',
+      client_id: client_id,
+      redirect_uri: redirect_uri,
+      state: state
+    },
+  })
+  .then((response) => {
+    console.log(response)
+  })
+  .catch((err) => console.log(err))
 
 const Footer = () => {
   return (
-    <div className="flex max-w-[1100px] flex-col pb-[40px]">
+    <div className="mx-auto flex max-w-[1100px] flex-col pb-[40px]">
       <div className="h-[1px] w-[1100px] bg-black"></div>
       <ul className="mb-[24px] flex justify-center pt-[40px]">
-        {options.map((option) => {
+        {options.map((option, index) => {
           return (
-            <li className="font-[15px] inline-block py-[8px] px-[16px] font-varela hover:text-5b8 hover:underline">
+            <li
+              key={index}
+              className="font-[15px] inline-block py-[8px] px-[16px] font-varela hover:text-5b8 hover:underline"
+            >
               <a href="/">{option}</a>
             </li>
-          );
+          )
         })}
       </ul>
 
@@ -18,7 +42,6 @@ const Footer = () => {
         <li className="py-[8px] px-[16px]">
           <a className="hover:text-5b8" href="https://twitter.com/baovtuber">
             <svg
-              class="twitter-icon"
               height="26px"
               width="26px"
               aria-hidden="true"
@@ -41,7 +64,6 @@ const Footer = () => {
               width="26px"
               height="26px"
               fill="currentColor"
-              class="bi bi-twitch"
               viewBox="0 0 16 16"
             >
               <path d="M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0H3.857zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142v6.286z"></path>
@@ -59,7 +81,6 @@ const Footer = () => {
               width="26px"
               height="26px"
               fill="currentColor"
-              class="bi bi-youtube"
               viewBox="0 0 16 16"
             >
               <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"></path>
@@ -68,7 +89,7 @@ const Footer = () => {
         </li>
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
